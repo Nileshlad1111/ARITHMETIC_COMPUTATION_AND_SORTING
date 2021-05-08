8502177521a638d64eefee29499953707b669ba5
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-08 09:03:17 +0530

    Modified:To print array using dictonary

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -27,11 +27,21 @@ arithmaticOperation[result2]=$result2
 arithmaticOperation[result3]=$result3
 
 #TO READ VALUE FROM THE DICTIONARY INTO THE ARRAY
-echo ${arithmaticOperation[@]}
+for((index=0; index<=${#arithmaticOperation[@]}; index++))
+do
+  array[index]=${arithmaticOperation[result$((index+1))]}
+done
+#TO PRINT
+echo "${array[@]}"
 
 #TO USE SORT COMMAND
+ascendingNumber=($(echo ${arithmaticOperation[*]} | tr " " "\n" | sort -n))
 decendingNumber=($(echo ${arithmaticOperation[*]} | tr " " "\n" | sort -nr))
 
 #TO PRINT THE DESENDING ORDER
 echo "Shorting in Descending Order"
 echo "Shorted Array is : ${decendingNumber[@]}"
+
+#TO PRINT THE ASENDING ORDER
+echo "Shorting in Ascending Order"
+echo "Shorted Array is : ${ascendingNumber[@]}"
